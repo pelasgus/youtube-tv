2d5449da5da0e5ef4d2e82a51fe8b5a02ec3abbe
J.A.A.Tsimouris <jim@tsimouris.com>
2023-04-12 22:40:49 +0300

    Rebased the project, now using rust via Tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+    .setup(|app| {
+        let window = app.get_window("main").unwrap();
+        window.eval("window.location.replace('https://www.youtube.com/tv/#')");
+        Ok(())
+      })
+      .run(tauri::generate_context!())
+      .expect("error running tauri app");
+}
