e96708781f3a66bfbe9930fe19561776a71d47f6
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-12-21 00:58:42 +0000

    Update and rename shell.nix to rust.nix

---
+++ etc/shells/nix/rust.nix
@@ -0,0 +1,19 @@
+# rust.nix
+# Author: D.A.Pelasgus
+
+# Rust User Shell
+let
+  # Unstable Channel | Rolling Release
+  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) { };
+  packages = with pkgs; [
+    pkg-config
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    trunk
+  ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+}
