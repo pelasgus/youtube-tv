e0e4d8a12b2475a7b41ce8eb6bec61df18087399
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-12-21 00:57:49 +0000

    Create tauri.nix
    Follows documentation dependencies but on unstable.

---
+++ etc/shells/nix/tauri.nix
@@ -0,0 +1,31 @@
+# tauri.nix
+# Author: D.A.Pelasgus
+
+let
+  # Unstable Channel | Rolling Release
+  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+}  
