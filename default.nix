2bfa77c421341f175a49eb6e8a62af1d96d18543
D.A.Pelasgus <demetrius@pelasgus.xyz>
2026-06-21 20:04:43 +0000

    bug fix: wry; gio dep
    https://github.com/tauri-apps/tauri/issues/11647

--- default.nix
+++ default.nix
@@ -1,21 +1,29 @@
-# default.nix
+# default.nix 
 # Author: D.A.Pelasgus
-
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "youtube-tv";
   version = "0.1.0";
 
-  src = ./.;  
+  src = ./.;
 
   cargoLock = {
     lockFile = ./Cargo.lock;
   };
 
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+  ];
+
+  postFixup = ''
+    wrapProgram $out/bin/youtube-tv \
+      --set GIO_MODULE_DIR ${pkgs.glib-networking}/lib/gio/modules
+  '';
+
   meta = with pkgs.lib; {
     description = "YouTube on TV | Cross-platform HTPC Leanback Launcher.";
     license = licenses.gpl3;
-    maintainers = with maintainers; [D.A.Pelasgus ];
+    maintainers = with maintainers; [ D.A.Pelasgus ];
   };
 }
